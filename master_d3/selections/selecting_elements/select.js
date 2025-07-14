98642c2623c65c53e4c8236573c16ed1d4af3232
Rock Jethwa <rockstar6789@gmail.com>
2025-07-14 19:43:29 -0400

    2nd commit; 9.32 part 1

---
+++ master_d3/selections/selecting_elements/select.js
@@ -0,0 +1,26 @@
+const h1 = d3.select('h1');
+console.log(h1);
+
+let firstSvg = d3.select("svg");
+console.log(firstSvg);
+firstSvg = d3.select("svg#circles");
+console.log(firstSvg);
+firstSvg = d3.select("#circles");
+console.log(firstSvg);
+
+let secondSvg = d3.select("#rects");
+console.log(secondSvg);
+secondSvg = d3.select("svg:nth-of-type(2)");
+console.log(secondSvg);
+
+let firstCircle = d3.select("circle");
+console.log(firstCircle);
+
+let secondCircle = d3.select("circle:nth-of-type(2)");
+console.log(secondCircle);	
+
+let firstRect = d3.select("rect:nth-of-type(1)");
+console.log(firstRect);
+
+let secondRect = d3.select("rect:nth-of-type(2)");
+console.log(secondRect);
